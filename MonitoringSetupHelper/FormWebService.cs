43157f7184ffa6192c0b9edd5342c604d778b24c
subba84 <128273110+subba84@users.noreply.github.com>
2023-10-18 18:54:51 +0530

    Add files via upload

---
+++ MonitoringSetupHelper/FormWebService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MonitoringSetupHelper
+{
+    public partial class FormWebService : Form
+    {
+
+        public FormWebService()
+        {
+            InitializeComponent();
+        }
+
+        private void FormSerialNumber_Load(object sender, EventArgs e)
+        {
+            radioButton1.Checked = true;
+            label1.Visible = false;
+            textBox1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            try
+            {
+                if (radioButton2.Checked)
+                {
+                    File.WriteAllText(Path.GetDirectoryName(Application.ExecutablePath) + "\\WebServiceUrl.txt", textBox1.Text);
+                }
+            }
+            catch (Exception ex)
+            {
+                using (EventLog eventLog = new EventLog("Application"))
+                {
+                    eventLog.Source = "Application";
+                    eventLog.WriteEntry(ex.Message + ex.StackTrace, EventLogEntryType.Information, 101, 1);
+                }
+
+            }
+            Application.Exit();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked)
+            {
+                label1.Hide();
+                textBox1.Hide();
+                
+            }
+
+          
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Show();
+            textBox1.Show();
+           
+        }
+    }
+}
---
+++ MonitoringTray/MonitoringForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace MonitoringTray
+{
+    partial class MonitoringForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Monitoring = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Monitoring);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1143, 677);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Monitoring
+            // 
+            this.Monitoring.Location = new System.Drawing.Point(4, 29);
+            this.Monitoring.Name = "Monitoring";
+            this.Monitoring.Padding = new System.Windows.Forms.Padding(3);
+            this.Monitoring.Size = new System.Drawing.Size(1135, 644);
+            this.Monitoring.TabIndex = 1;
+            this.Monitoring.Text = "Monitoring";
+            this.Monitoring.UseVisualStyleBackColor = true;
+            // 
+            // MonitoringForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1143, 677);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "MonitoringForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Monitoring";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Monitoring;
+    }
+}
+
